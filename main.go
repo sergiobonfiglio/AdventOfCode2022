bed0f6d74a570186f50bb1d7ab0e1374453257f3
Sergio Bonfiglio <sergio.bonfiglio.extern@dertouristik.com>
2022-12-08 10:22:55 +0100

    refactoring main

--- main.go
+++ main.go
@@ -4,17 +4,21 @@ import (
 	"AdventOfCode2022/day1"
 	"AdventOfCode2022/day2"
 	"AdventOfCode2022/day3"
+	"AdventOfCode2022/day4"
 	"fmt"
 )
 
 func main() {
 
-	fmt.Println("Day 1")
-	day1.SolveDay()
+	var solvers = []func(){
+		day1.SolveDay,
+		day2.SolveDay,
+		day3.SolveDay,
+		day4.SolveDay,
+	}
 
-	fmt.Println("Day 2")
-	day2.SolveDay()
-
-	fmt.Println("Day 3")
-	day3.SolveDay()
+	for i, solver := range solvers {
+		fmt.Printf("\n== Day %d ==\n", i+1)
+		solver()
+	}
 }
